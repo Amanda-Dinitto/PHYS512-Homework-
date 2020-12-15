f5100560633dab9dcd9a11baae3fc194cee67b70
Amanda-Dinitto <63658932+Amanda-Dinitto@users.noreply.github.com>
2020-12-14 23:05:25 -0500

    Update Project.py

--- Project/Project.py
+++ Project/Project.py
@@ -16,23 +16,22 @@ def greens_fn(x, y, z, soft = 0.01):
             pot = 1/(4*np.pi*r) ## Laplacian in 3D 
     return pot 
 
-def density_grid(particles, cells):
-    grid=np.zeros(cells)
-    cell_length=1.0/cells
-    left_index_f = 0.0
-    cell_position= 0.0
-    for i in range(particles.shape[0]):
-        left_index=particles[i]
-        (left_index_f,cell_position)=divmod((particles[i]),cell_length)
-        cell_position = cell_position/cell_length
-        left_index=int(left_index_f)
-        grid[left_index % cells]+=1-cell_position
-        grid[(left_index + 1) % cells]+=cell_position
-    return np.array([grid])
+def density_grid(x, y, z, n):
+    points = (x,y,z)
+    #print(points)
+    grid_min = -n
+    grid_max = n
+    n_grid = 2*n
+    H, edges = np.histogramdd(points, bins = n_grid, range=((grid_min, grid_max), (grid_min, grid_max), (grid_min, grid_max)))
+    edges_x = edges[0]
+    edges_y = edges[1]
+    edges_z = edges[2] 
+    return H 
+    
 
 def get_potential(x, y, z, n, soft = 0.01):
    greens = greens_fn(x, y, z, soft = 0.01)
-   rho = density_grid(x, n)
+   rho = density_grid(x, y, z, n)
    G = np.fft.fftn(greens)
    r = np.fft.fftn(rho)
    potential = np.fft.ifftn(G*r)
@@ -40,14 +39,17 @@ def get_potential(x, y, z, n, soft = 0.01):
 
 def get_force(x, y, z, m, n, soft = 0.01):
     pot = get_potential(x, y, z, n, soft = 0.01)
-    ax, ay, az = np.gradient(pot, 3) 
+    ax = np.gradient(pot, axis=0) 
+    #print(pot)
+    ay = np.gradient(pot, axis=1)
+    az = np.gradient(pot, axis=2)
     fx = ax*m
     fy = ay*m
     fz = az*m
     return fx, fy, fz, pot  
 
 def take_leapfrog_step(x, y, z, vx, vy, vz, dt, m, n):
-    m = m*np.ones(n)  ##change this 
+    #m = m*np.ones(n)  ##change this 
     """take half step"""
     xx = x + 0.5*vx*dt
     yy = y + 0.5*vy*dt
@@ -63,26 +65,26 @@ def take_leapfrog_step(x, y, z, vx, vy, vz, dt, m, n):
     vx = vx + dt*fx
     vy = vy + dt*fy
     vz = vz + dt*fz
-    KE = 0.5*np.sum(m*(vx**2 + vy**2 + vz**2)) 
-    return x,y,z,pot, KE
+    return x,y,z, vx, vy, vz, pot
 
 
 """
 #Part A: Single particle at rest
 """
-n = 1 
-m = 1.0
-x = np.random.randn(n)
-y = np.random.randn(n)
-z = np.random.randn(n)
+n = 1
+m = 1
+x = np.array([0])
+y = np.array([0])
+z = np.array([0])
 vx = 0.0*x #velocity is zero 
 vy = 0.0*y
 vz = 0.0*z
 soft = 0.01
-dt = soft**1.5 ##dt given by v_max/a_max = soft**1.5
+dt = soft**1.5 ##dt given by v_max/a_max = soft**-1.5
 
-for i in range(10):
-    x,y,z,pot, kinetic = take_leapfrog_step(x,y,z,vx,vy,vz,dt, m, n)
+for iter in range(10):
+    x,y,z, vx, vy, vz, pot = take_leapfrog_step(x,y,z,vx,vy,vz,dt, m, n)
+    #KE = 0.5*np.sum(m*(vx**2 + vy**2 + vz**2)) 
 ##now plt particle position over time 
     
 """
@@ -102,3 +104,4 @@ v = np.sqrt((g*(m1+m2))/r) ## velocity in circular motion
 v = 0.0*v  ??? how to separate the velocity components?
 dt = soft**1.5*0.05 ##dt given by v_max/a_max = soft**1.5
 """
+
