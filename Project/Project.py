dee60325a21d41f7e66e935f3faa75bc27678b68
Amanda-Dinitto <63658932+Amanda-Dinitto@users.noreply.github.com>
2020-12-17 15:07:50 -0500

    Update Project.py

--- Project/Project.py
+++ Project/Project.py
@@ -30,19 +30,17 @@ def get_potential(x, y, z, n):
    G = np.fft.fftn(greens)
    r = np.fft.fftn(rho)
    potential = np.fft.ifftn(G*r)
-   return potential, greens
+   return potential
 
 def get_force(x, y, z, m, n, soft = 0.1):
-    potential, greens = get_potential(x, y, z, n)
+    pot= get_potential(x, y, z, n)
     #print(np.real(x))
     for i in range(len(x)):
         bins = np.linspace(-2,2,1)
         print(np.real(x[i]), np.real(y[i]), np.real(z[i]))
         particle_x_bins = np.digitize(np.real(x), bins, right=True)
-        #print(particle_x_bins)
         particle_y_bins = np.digitize(np.real(y), bins, right=True)
         particle_z_bins = np.digitize(np.real(z), bins, right=True)
-        pot = potential - np.roll(greens, [particle_x_bins[i],particle_y_bins[i],particle_z_bins[i]], axis = (0,1,2))
         dx = np.gradient(pot, axis=0) 
         dy = np.gradient(pot, axis=1)
         dz = np.gradient(pot, axis=2)
@@ -60,28 +58,7 @@ def get_force(x, y, z, m, n, soft = 0.1):
         fx = (ax)*m
         fy = (ay)*m
         fz = (az)*m
-    return fx, fy, fz  
-
-def take_leapfrog_step(x, y, z, vx, vy, vz, dt, m, n):
-    """set up arrays to store info"""
-    for i in range(len(x)):
-        """take half step"""
-        xx = x[i] + 0.5*vx*dt
-        yy = y[i] + 0.5*vy*dt
-        zz = z[i] + 0.5*vz*dt
-        fx, fy, fz = get_force(xx, yy, zz, m, n, soft = 0.01)
-        vvx = vx + 0.5*dt*fx
-        vvy = vy + 0.5*dt*fy
-        vvz = vz + 0.5*dt*fz
-        """update all values"""
-        x = x[i] + dt*vvx
-        y = y[i] + dt*vvy
-        z = z[i] + dt*vvz
-        vx = vx + dt*fx
-        vy = vy + dt*fy
-        vz = vz + dt*fz
-        KE = 0.5*np.sum(m*(vx**2 + vy**2 + vz**2)) 
-    return x,y,z, vx, vy, vz
+    return fx, fy, fz, pot  
 
 def take_leapfrog_step(x, y, z, vx, vy, vz, dt, m, n):
     """set up arrays to store info"""
@@ -89,7 +66,7 @@ def take_leapfrog_step(x, y, z, vx, vy, vz, dt, m, n):
     xx = x + 0.5*vx*dt
     yy = y + 0.5*vy*dt
     zz = z + 0.5*vz*dt
-    fx, fy, fz = get_force(xx, yy, zz, m, n, soft = 0.01)
+    fx, fy, fz, pot = get_force(xx, yy, zz, m, n, soft = 0.01)
     vvx = vx + 0.5*dt*fx
     vvy = vy + 0.5*dt*fy
     vvz = vz + 0.5*dt*fz
@@ -101,7 +78,7 @@ def take_leapfrog_step(x, y, z, vx, vy, vz, dt, m, n):
     vy = vy + dt*fy
     vz = vz + dt*fz
     KE = 0.5*np.sum(m*(vx**2 + vy**2 + vz**2)) 
-    return x,y,z, vx, vy, vz
+    return x,y,z, vx, vy, vz, pot
 """
 #Part A: Single particle at rest
 
@@ -133,36 +110,39 @@ for iter in range(20):
 #Part B: 2 Particles in Circular Orbit
 #m1v1 = m2v2 for circular motion (or v1/r1 = v2/r2)
 ##work in progress still
-
 n = 10
 m = 1.0
-x = np.array([1,1])
-y = np.array([0,1])
-z = np.array([1,1])
+x = np.array([1,2])
+y = np.array([1,2])
+z = np.array([1,1]) ##same z plane so they will orbit only in x and y 
 #velocities should be equal since mass is equal start at zero 
 ##vx = np.zeros([2])
 #vy = np.zeros([2])
 #vz = np.zeros([2])
-vx = x*0
-vy = y*0
-vz = z*0
+vx = np.array([1,1])*0.25
+vy = np.array([1,1])*0.25
+vz = np.array([1,1])*0.25
 
 soft = 0.01
-dt = soft**1.5*0.05 ##dt given by v_max/a_max = soft**1.5
-for i in range(5):
-    #for iter in range(10): ##taking 10 steps between plots 
-    x,y,z, vx, vy, vz = take_leapfrog_step(x,y,z,vx,vy,vz,dt, m, n)
+dt = soft**1.5*0.5 ##dt given by v_max/a_max = soft**1.5
+plt.ion()
+fig = plt.figure()
+ax = fig.add_subplot(111,projection='3d')
+ax.set_xlim(0,4)
+ax.set_ylim(0,4)
+ax.set_zlim(0,4)
+#plt.pause(0.1)
+for i in range(50):
+    x,y,z, vx, vy, vz, pot = take_leapfrog_step(x,y,z,vx,vy,vz,dt, m, n)
     KE = 0.5*np.sum(m*(vx**2 + vy**2 + vz**2)) 
-    #print(np.real(x))
 ##now plt particle position over time 
-    plt.ion()
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    ax.scatter3D(np.real(x), np.real(y), np.real(z))
-    plt.title('2 Particles after iteration '+ str(i))
-    ##set limits
+    ax.scatter3D(np.real(x[0]), np.real(y[0]), np.real(z[0]))
+    ax.scatter3D(np.real(x[1]), np.real(y[1]), np.real(z[1]))
+    #plt.title('2 Particles after iteration '+ str(i))
     #plt.savefig('2 points circular iteration'+ str(iter)+'.png')
-    plt.show()
+plt.show()
+
+
 
 """
 Part C: Many Particles with Periodic and Non-Periodic Boundary Conditions
